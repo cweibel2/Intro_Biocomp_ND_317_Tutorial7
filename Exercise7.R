01c0ade3bfc18f6e0540c88c8d25ed1807aa49f6
Chelsea Weibel <cweibel2@gmail.com>
2017-10-06 16:42:10 -0400

    Added question 1

--- Exercise7.R
+++ Exercise7.R
@@ -1,3 +1,54 @@
+# Question 1
+
+#Uploading data
+setwd("~/Desktop/Biocomputing/Week6")
+fasta=scan(file = "Lecture11.fasta.txt",what=character())
+
+# Using Stuart's code to get my matrix values becuase my code 
+# is still kind of weird
+
+# pre-allocate matrix to store sequence information
+summ=matrix(NA,length(fasta)/2,4)
+
+# loop through lines of fasta and process each record
+for(i in 1:length(fasta)){
+  # if-else to figure out if we have a recordID line or sequence line
+  if(grepl(">",fasta[i])){
+    summ[((i+1)/2),1]=substr(fasta[i],2,nchar(fasta[i])) # could use counter instead of complicated row calculation
+  }else{
+    seqLength=nchar(fasta[i])
+    
+    numG=nchar(fasta[i])-nchar(gsub("G","",fasta[i]))
+    numC=nchar(fasta[i])-nchar(gsub("C","",fasta[i]))
+    if(seqLength<=14){
+      Tm=2*(numG+numC)+2*seqLength
+    }else{
+      Tm=-9999
+    }
+    
+    summ[i/2,2]=seqLength
+    summ[i/2,3]=round((numG+numC)/seqLength*100,1)
+    summ[i/2,4]=Tm
+  }
+}
+
+seqSumm=data.frame(sequenceID=summ[,1],sequenceLength=as.numeric(summ[,2]),percentGC=as.numeric(summ[,3]),meltingTemp=as.numeric(summ[,4]))
+
+#### Graph 1 - Histogram of sequence lengths
+
+a = ggplot(data = seqSumm, aes(x = sequenceLength))
+a + geom_histogram() + theme_classic() + stat_bin(binwidth = 1) + 
+  xlab("Sequence Length") + ylab("Count") + 
+  ggtitle("Histogram of Sequence Length")
+
+#### Graph 2 - Histogram of %GC Content
+
+b  = ggplot(data = seqSumm, aes(x = percentGC))
+b + geom_histogram() + theme_classic() + xlab("% GC Content") + 
+  ylab("Count") + ggtitle("Histogram of %GC Content")
+
+
+
 #Question 2
 #Point-framing data and LAI data for a site in Alaska. 
 #Point-framing is just a measure of species richness from the number of times a pin dropped from the top of the canopy hits a certain spp.
@@ -60,4 +111,5 @@ d + geom_bar(aes(x = as.factor(region), y = observations), stat = "summary",
 a <- ggplot(data = data)
 a + theme_classic() + geom_jitter(aes(x=as.factor(region),y=observations), alpha=0.1) + labs(x='Region', y='Observations') + ggtitle("Observations in Each Region")
 #Yes because the bar plot compares the averages of each region, 
-#while the geom_jitter plot gives you an idea of the average and the spread because it shows each individual data point
+#while the geom_jitter plot gives you an idea of the average and 
+#the spread because it shows each individual data point
